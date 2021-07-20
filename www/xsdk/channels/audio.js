8f1a2977d13a5a55bdc67840b51bb85f28e4c865
Jim Kroon <info@jimkroon.nl>
2021-07-20 18:56:03 +0200

    Added experimental audio support

--- www/xsdk/channels/audio.js
+++ www/xsdk/channels/audio.js
@@ -1,10 +1,26 @@
+// if(typeof require != "undefined" && typeof libopus == "undefined"){
+//     LIBOPUS_WASM_URL = "assets/opus/libopus.wasm";
+//     libopus = require("assets/opus/libopus.wasm.js");
+// }
+
+console.log('LIBOPUS: ', libopus)
+
 class AudioChannel extends BaseChannel {
 
-    #frameQueue = {}
+    #frameBuffer = ''
+    #frameBufferQueue = []
+    #frameBufferSize = 1440 //1440 = 0.2 - 4800 = 1 - 7200 = 1.5
 
     #audioContext = null
+    #gainNode = null
+    #audioOffset = null
+    #audioFrames = 0
 
     #packetCounter = 0
+    #processedCounter = 0
+
+    #opusDecoder = null
+    #worker = null
 
     #events = {
         'fps': [],
@@ -14,10 +30,16 @@ class AudioChannel extends BaseChannel {
     onOpen(event) {
         setInterval(() => {
             // console.log('xSDK channels/video.js - [performance] frameQueue size:', Object.keys(this.#frameQueue).length, 'frameMetadataQueue size:', this.#frameMetadataQueue.length)
-            var fps = this.#packetCounter
-            this.emitEvent('queue', { frameQueue: Object.keys(this.#frameQueue).length, packetCounter: this.#packetCounter, fps: fps })
+            var fps = this.#processedCounter
+            this.emitEvent('queue', {
+                frameBuffer: this.#frameBuffer.length,
+                frameBufferQueue: this.#frameBufferQueue.length,
+                packetCounter: this.#packetCounter,
+                fps: fps
+            })
             
             this.#packetCounter = 0
+            this.#processedCounter = 0
             // this.#frameCounter = 0
 
             this.emitEvent('fps', { fps: fps })
@@ -30,6 +52,48 @@ class AudioChannel extends BaseChannel {
             sampleRate: 48000, //def = 44100,
         });
 
+        // For volume? See https://developer.mozilla.org/en-US/docs/Web/API/GainNode
+        this.#gainNode = this.#audioContext.createGain(),
+        this.#gainNode.connect(this.#audioContext.destination)
+
+        // // var audioBuffer = this.#audioContext.createBuffer(2, 960, 48000)
+        // this.#audioBuffer = this.#audioContext.createBuffer(2, (48000 * 3), 48000) // Why 1440? (targetAudioBufferSize)
+
+        // for (var channel = 0; channel < this.#audioBuffer.numberOfChannels; channel++) {
+        //     // This gives us the actual array that contains the data
+        //     var nowBuffering = this.#audioBuffer.getChannelData(channel);
+        //     for (var i = 0; i < this.#audioBuffer.length; i++) {
+        //       // Math.random() is in [0; 1.0]
+        //       // audio needs to be in [-1.0; 1.0]
+        //       nowBuffering[i] = Math.random() * 2 - 1;
+        //     }
+        // }
+
+        // var source = this.#audioContext.createBufferSource();
+
+        // // set the buffer in the AudioBufferSourceNode
+        // source.buffer = audioBuffer;
+        
+        // // connect the AudioBufferSourceNode to the
+        // // destination so we can hear the sound
+        // source.connect(this.#audioContext.destination);
+        
+        // // start the source playing
+        // source.start();
+
+
+        // Init new OPUS worker
+        this.#worker = new Worker('assets/opus/opusworker.js');
+        this.#worker.onmessage = function(e) {
+            // console.log('Message received from worker:', e.data);
+        }
+
+        this.#opusDecoder = new libopus.Decoder(2, 48000);
+
+        setInterval(() => {
+            this.sendToMediasource()
+        }, 10)
+
         // var gainNode = this.#audioContext.createGain()
         // gainNode.connect(this.audioContext.destination)
     }
@@ -37,7 +101,7 @@ class AudioChannel extends BaseChannel {
     onMessage(event) {
         this.#packetCounter++
 
-        if(this.getQueueLength() < 50) {
+        // if(this.getQueueLength() < 100) {
 
             var messageBuffer = new DataView(event.data);
             var frameId = messageBuffer.getUint32(0, true);
@@ -47,7 +111,7 @@ class AudioChannel extends BaseChannel {
 
             var frameData = new Uint8Array(event.data, 12)
 
-            this.addToQueue(event)
+            // this.addToQueue(event)
 
             this.processFrame({
                 frameId: frameId,
@@ -57,33 +121,120 @@ class AudioChannel extends BaseChannel {
                 // serverDataKey: serverDataKey,
                 frameData: frameData
             })
-        }
+        // }
     }
 
     processFrame(frameData) {
-        var frameId = frameData.frameId
-        console.log('xSDK channels/audio.js - Create frameId '+frameId+' and insert data:', frameData)
+        // var frameId = frameData.frameId
+        // // console.log('xSDK channels/audio.js - Create frameId '+frameId+' and insert data:', frameData)
 
-        if(this.#frameQueue[frameId] !== undefined){
-            throw 'This should not happen!! pikachu :O'
-        } else {
-            this.#frameQueue[frameId] = {
-                frameId: frameId,
-                timestamp: frameData.timestamp,
-                frameSize: frameData.frameSize,
-                // frameOffset: frameData.frameOffset,
-                frameData: frameData.frameData
+        // if(this.#frameQueue[frameId] !== undefined){
+        //     throw 'This should not happen!! pikachu :O'
+        // } else {
+        //     this.#frameQueue[frameId] = {
+        //         frameId: frameId,
+        //         timestamp: frameData.timestamp,
+        //         frameSize: frameData.frameSize,
+        //         // frameOffset: frameData.frameOffset,
+        //         frameData: frameData.frameData
+        //     }
+
+            // this.sendToMediasource()
+            this.decodeStream(frameData.frameData)
+
+            // if(this.#frameBuffer.length > this.#frameBufferSize) {
+            for(; this.#frameBuffer.length > this.#frameBufferSize;){
+                // Lets split up the packets
+                // fetch x size
+
+                // this.#opusDecoder.postMessage({
+
+                // });
+
+                // console.log('outputBuffer1', this.#frameBuffer.slice(0, this.#frameBufferSize).length)
+                var outputBuffer = this.str2ab(this.#frameBuffer.slice(0, this.#frameBufferSize))
+                // console.log('outputBuffer2', outputBuffer)
+                var outputBuffer = new Int16Array(outputBuffer)
+                // console.log('outputBuffer3', outputBuffer)
+                // console.log('before framebuffer', this.#frameBuffer.length)
+                this.#frameBuffer = this.#frameBuffer.slice(this.#frameBufferSize)
+
+                // console.log('after framebuffer', this.#frameBuffer.length)
+
+                // for(var i = 0; i < this.#frameBufferSize; i++){
+                //     outputBuffer[i] = this.#frameBuffer.shift()
+                // }
+                // var outputBuffer = this.#frameBuffer.splice(0, this.#frameBufferSize)
+
+                this.#frameBufferQueue.push(outputBuffer)
+                // console.log('created frameBufferQueue')
+                // this.sendToMediasource() // This here will cause performance issues.
             }
+            // } else {
+            //     // Wait for more packets.
+            //     console.log('wait with frameBufferQueue')
+            // }
 
-            this.sendToMediasource()
-            // var audio = this.decodeStream(frameData.frameData)
-        }
+        // }
 
     }
 
     decodeStream(data) {
         // var worker = this.worker()
         // console.log('worker:', worker)
+
+        // Things we know:
+        // - Codec is opus
+        // - SampleRateHz is 24000 (20000+4000)
+        // - Each frame we get is 20ms of sound
+
+        // console.log('INPUT:', data)
+        this.#opusDecoder.input(data)
+        var output = ''
+
+        while(output = this.#opusDecoder.output()){
+
+            // // var audioBuffer = this.#audioContext.createBuffer(2, 960, 48000)
+            // var audioBuffer = this.#audioContext.createBuffer(2, 1440, 48000) // 1440? (targetAudioBufferSize) (960 * 1.5)
+
+            // if(audioBuffer.numberOfChannels != 2){
+            //     throw 'audioBuffer.numberOfChannels is not 2.. Cannot process audio...'
+            // }
+
+            // var leftChannel = audioBuffer.getChannelData(0);
+            // var rightChannel = audioBuffer.getChannelData(0);
+
+            // var floatValues = []
+            // for (var i = 0; i < output.length; i++) {
+            //     // Math.random() is in [0; 1.0]
+            //     // audio needs to be in [-1.0; 1.0]
+            //     var value = output[i]
+            //     value /= 32767;
+            //     floatValues[i] = value
+
+            //     if(! (i % 2)) {
+            //         var channel = leftChannel
+            //     } else {
+            //         var channel = rightChannel
+            //     }
+                
+            //     channel[i] = floatValues[i]
+            // }
+            // console.log('float:', floatValues)
+
+            // var source = this.#audioContext.createBufferSource()
+            // source.buffer = audioBuffer
+            // source.connect(this.#audioContext.destination)
+            
+            // source.start()
+
+            /** Refactored block */
+            output = this.ab2str(output)
+            // console.log('DECODED:', output)
+            this.#frameBuffer += output
+        }
+
+        // this.#opusDecoder.postMessage(data);
 
         // this.#audioContext.decodeAudioData(data.buffer, (event) => {
         //     console.log('DECODE:', event)
@@ -95,16 +246,91 @@ class AudioChannel extends BaseChannel {
     sendToMediasource() {
         var client = this.getClient()
 
-        if(client.getAudioSource().updating === false){
-            var nextFrame = Object.keys(this.#frameQueue)[0]
-            var frame = this.#frameQueue[nextFrame]
+        if(this.#audioContext.state === 'running'){
+            // console.log('AudioSource is running:', this.#audioContext.state)
 
-            console.log(frame)
-            
-            delete this.#frameQueue[nextFrame]
+            // var outputBuffer = [];
+            // if(this.#frameBuffer.length > this.#frameBufferSize){
+            if(this.#frameBufferQueue.length > 0){
 
-            // client.getAudioSource().appendBuffer(frame.frameData);
+                // Set audio offset
+                if(this.#audioOffset === null){
+                    // this.#audioOffset = this.#audioContext.currentTime
+                    this.#audioOffset = 0
+                }
+                
+                var outputBuffer = this.#frameBufferQueue.shift()
+                var frameCount = 1
+
+                // var outputBuffer = []
+                // var frameCount = 0;
+                // for(;this.#frameBufferQueue.length > 0;){
+                //     var outputBuffer = outputBuffer.concat(this.#frameBufferQueue.shift())
+                //     frameCount++
+                // }
+
+                this.#audioFrames += frameCount
+                this.#audioOffset += (frameCount * 0.02) // one frame is 20ms
+
+                // console.log(outputBuffer);
+                // outputBuffer = this.str2ab(outputBuffer)
+
+                // console.log('this.#frameBuffer:', this.#frameBuffer)
+                // console.log('outputBuffer:', outputBuffer)
+
+                // Create audiostream
+                var audioBuffer = this.#audioContext.createBuffer(2, (this.#frameBufferSize*frameCount), 48000) // 1440? (targetAudioBufferSize) (960 * 1.5)
+
+                if(audioBuffer.numberOfChannels != 2){
+                    throw 'audioBuffer.numberOfChannels is not 2.. Cannot process audio...'
+                }
+
+                var leftChannel = audioBuffer.getChannelData(0);
+                var rightChannel = audioBuffer.getChannelData(0);
+
+                // console.log('play audio: raw', outputBuffer)
+                outputBuffer = Int16Array.from(outputBuffer)
+                // console.log('play audio: int16', outputBuffer)
+                var outputBuffer = this.arrayIntToFloat(outputBuffer)
+                // console.log('play audio: float', outputBuffer)
+                this.#processedCounter++;
+
+                for (var i = 0; i < outputBuffer.length; i++) {
+                    if(! (i % 2)) {
+                        var channel = leftChannel
+                    } else {
+                        var channel = rightChannel
+                    }
+                    
+                    channel[i] = outputBuffer[i]
+                }
+
+                var source = this.#audioContext.createBufferSource()
+                source.buffer = audioBuffer
+                source.connect(this.#gainNode)
+                
+                // console.log('AudioContext:', this.#audioContext.currentTime, this.#audioOffset)
+                // source.start(this.#audioOffset)
+                source.start()
+                // console.log('aaaaand play source!')
+                
+            } else {
+                // console.log('Framebuffer not filled yet. skipping...')
+            }
+
+        } else {
+            // console.log('AudioSource is not running:', this.#audioContext.state)
         }
+    }
+
+    arrayIntToFloat(intArray){
+        var floatValues = []
+        for (var i = 0; i < intArray.length; i++) {
+            var value = intArray[i]
+            value /= 32767;
+            floatValues[i] = value
+        }
+        return floatValues
     }
 
     addEventListener(name, callback) {
@@ -115,6 +341,19 @@ class AudioChannel extends BaseChannel {
         for(var callback in this.#events[name]){
             this.#events[name][callback](event)
         }
+    }
+
+    ab2str(buf) {
+        return String.fromCharCode.apply(null, new Uint16Array(buf));
+    }
+
+    str2ab(str) {
+        var buf = new ArrayBuffer(str.length*2); // 2 bytes for each char
+        var bufView = new Uint16Array(buf);
+        for (var i=0, strLen=str.length; i < strLen; i++) {
+            bufView[i] = str.charCodeAt(i);
+        }
+        return buf;
     }
     
 }
