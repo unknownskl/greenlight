7b47a4fdb993baf7a788781f98143f2bcfa487a7
Jim Kroon <info@jimkroon.nl>
2022-11-04 21:10:57 +0100

    Potentially fix xCloud error on startup #511

--- main/helpers/xcloudbrowser.ts
+++ main/helpers/xcloudbrowser.ts
@@ -107,6 +107,6 @@ export default class xCloudBrowser {
             ids = inputId
         }
 
-        return this._webApi.getProvider('catalog').get('/v7.0/products?actionFilter=Browse&bigIds='+ids+'&fieldsTemplate=browse&languages=en-us&market='+this._market)
+        return this._webApi.getProvider('catalog').get('/v7.0/products?actionFilter=Browse&bigIds='+ids+'&fieldsTemplate=browse&languages=en-us&market=us')
     }
 }
