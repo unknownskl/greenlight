c8f6ac063a45a0a3ede7c5d7eed78d64e48404d3
Jim Kroon <info@jimkroon.nl>
2023-05-31 23:11:46 +0200

    Added debug graphs in debug window #828

--- renderer/components/ui/streamcomponent.tsx
+++ renderer/components/ui/streamcomponent.tsx
@@ -28,8 +28,8 @@ function StreamComponent({
   let debugElement = document.getElementById('component_streamcomponent_debug')
   let webRtcStatsInterval
 
-  let jitterData = [[Date.now()], [0.0]]
-  let droppedData = [[Date.now()], [0.0], [0.0]]
+  let jitterData = [new Float32Array([Date.now()]), new Float32Array([0.0])]
+  let droppedData = [new Float32Array([Date.now()]), new Float32Array([0.0]), new Float32Array([0.0])]
   let framesDroppedBaseline = 0
   let packetsDroppedBaseline = 0
   let frameCountDomUpdate = 0
@@ -54,29 +54,17 @@ function StreamComponent({
       series: [
         {},
         {
-          // initial toggled state (optional)
           show: true,
           spanGaps: false,
-    
-          // in-legend display
           label: "Jitter",
-    
-          // series style
           stroke: "red",
           width: 2,
           fill: "rgba(255, 0, 0, 0.3)",
-          // dash: [10, 5],
         }
       ],
-      // scales: {
-      //   x: {
-      //     time: false,
-      //   },
-      // },
       axes: [
         {},
         {
-          // scale: "ms",
           values: (u, vals, space) => vals.map(v => +v.toString() + " ms"),
         }
       ]
@@ -90,50 +78,29 @@ function StreamComponent({
       series: [
         {},
         {
-          // initial toggled state (optional)
           show: true,
           spanGaps: false,
-    
-          // in-legend display
           label: "Packets lost",
-    
-          // series style
           stroke: "green",
           width: 2,
           fill: "rgba(0, 255, 0, 0.3)",
-          // dash: [10, 5],
         },
         {
-          // initial toggled state (optional)
           show: true,
           spanGaps: false,
-    
-          // in-legend display
           label: "Frames dropped",
-    
-          // series style
           stroke: "blue",
           width: 2,
           fill: "rgba(0, 0, 255, 0.3)",
-          // dash: [10, 5],
         }
       ],
-      // scales: {
-      //   x: {
-      //     time: false,
-      //   },
-      // },
       axes: [
         {},
         {
-          // side: 1,
-          // scale: "count",
           values: (u, vals, space) => vals.map(v => +v.toFixed(0) + " dropped"),
           grid: {show: false},
         },
         {
-          // side: 1,
-          // scale: "count",
           grid: {show: false},
         },
       ]
@@ -153,12 +120,12 @@ function StreamComponent({
               droppedData = sliceData(droppedData, droppedData[0].length-1200, droppedData[0].length)
             }
 
-            jitterData[0].push(Date.now())
-            jitterData[1].push(report['jitter'])
+            jitterData[0] = new Float32Array([...Array.from(jitterData[0]), Date.now()])
+            jitterData[1] = new Float32Array([...Array.from(jitterData[0]), report['jitter']])
 
-            droppedData[0].push(Date.now())
-            droppedData[1].push(report['packetsLost']-packetsDroppedBaseline)
-            droppedData[2].push(report['framesDropped']-framesDroppedBaseline)
+            droppedData[0] = new Float32Array([...Array.from(droppedData[0]), Date.now()])
+            droppedData[1] = new Float32Array([...Array.from(droppedData[0]), report['packetsLost']-packetsDroppedBaseline])
+            droppedData[2] = new Float32Array([...Array.from(droppedData[0]), report['framesDropped']-framesDroppedBaseline])
             packetsDroppedBaseline = report['packetsLost']
             framesDroppedBaseline = report['framesDropped']
 
