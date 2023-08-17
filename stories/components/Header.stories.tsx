fce0c4d8baaee4bb2e8cce51412f3ea140afb02e
Jim Kroon <info@jimkroon.nl>
2023-08-17 18:06:47 +0200

    Remove storybook

--- stories/components/Header.stories.tsx
+++
@@ -1,9 +0,0 @@
-import Header from "../../renderer/components/header";
-
-export default {
-  title: "Components/Header",
-  component: Header,
-};
-
-// export const HeaderNavigation = () => <Header />
-export const HeaderNavigation = (args) => <Header {...args} />
--- stories/pages/Home.stories.tsx
+++
@@ -1,9 +0,0 @@
-import Home from "../../renderer/pages/home";
-
-export default {
-  title: "Pages/Home",
-  component: Home,
-};
-
-export const HomePage = () => <Home />
-
