222677a08560055e6a03b0f01c7026ae3fad265b
Jim Kroon <info@jimkroon.nl>
2023-09-14 21:51:02 +0200

    Fix build

--- renderer/pages/achievements/[titleid].tsx
+++ renderer/pages/achievements/[titleid].tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
 import { useRouter } from 'next/router'
-import { ipcRenderer } from 'electron'
+// import { ipcRenderer } from 'electron'
 import xCloudPlayer from 'xbox-xcloud-player'
 
 import Header from '../../components/header'
@@ -19,39 +19,39 @@ function AchievmentTitle() {
   React.useEffect(() => {
 
     if(achievements.length === 0){
-      ipcRenderer.send('xboxweb', {
-        type: 'get_recent_achievements_titleid',
-        titleid: router.query.titleid,
-      })
+      // ipcRenderer.send('xboxweb', {
+      //   type: 'get_recent_achievements_titleid',
+      //   titleid: router.query.titleid,
+      // })
       loadingAchievements = []
     }
 
-    ipcRenderer.on('xboxweb', (event, args) => {
-      if(args.type === 'error'){
-        alert((args.data !== undefined) ? args.message+': '+JSON.stringify(args.data) : args.message)
+    // ipcRenderer.on('xboxweb', (event, args) => {
+    //   if(args.type === 'error'){
+    //     alert((args.data !== undefined) ? args.message+': '+JSON.stringify(args.data) : args.message)
 
-      } else if(args.type == 'get_recent_achievements_titleid') {
-        console.log('Received achievements:', args)
+    //   } else if(args.type == 'get_recent_achievements_titleid') {
+    //     console.log('Received achievements:', args)
 
-        loadingAchievements = [...loadingAchievements, ...args.data.achievements]
+    //     loadingAchievements = [...loadingAchievements, ...args.data.achievements]
 
-        if(args.data.pagingInfo.continuationToken !== null){
-          ipcRenderer.send('xboxweb', {
-            type: 'get_recent_achievements_titleid',
-            titleid: router.query.titleid,
-            continuationToken: args.data.pagingInfo.continuationToken,
-          })
-        } else {
-          setAchievements(loadingAchievements)
-        }
+    //     if(args.data.pagingInfo.continuationToken !== null){
+    //       ipcRenderer.send('xboxweb', {
+    //         type: 'get_recent_achievements_titleid',
+    //         titleid: router.query.titleid,
+    //         continuationToken: args.data.pagingInfo.continuationToken,
+    //       })
+    //     } else {
+    //       setAchievements(loadingAchievements)
+    //     }
 
-      } else {
-        console.log('got unknown response:', args)
-      }
-    })
+    //   } else {
+    //     console.log('got unknown response:', args)
+    //   }
+    // })
 
     return () => {
-      ipcRenderer.removeAllListeners('xboxweb')
+      // ipcRenderer.removeAllListeners('xboxweb')
     }
   })
 
