d0b2e1b084fafeccbdc7bc484d65a8b90db3a7e3
Originalimoc <wzj9912@icloud.com>
2023-10-17 17:00:46 +0800

    Run from flatpak folder directly

--- flatpak/build_bundle_manually.sh
+++ flatpak/build_bundle_manually.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo Ensure Rust is installed, then Enter
+echo Run this under flatpak folder. Ensure Rust is installed. Press Enter to continue
 read confirm
 sudo pacman -Syu
 sudo pacman -S python-aiohttp python-toml python-pipx flatpak flatpak-builder yarn npm nodejs base-devel git
@@ -9,26 +9,22 @@ git clone https://github.com/flatpak/flatpak-builder-tools
 cd flatpak-builder-tools/
 fbtPath=$(pwd)
 pipx install ./node --force
-export PATH=${PATH}:/home/imoc/.local/bin
+export PATH=${PATH}:${HOME}/.local/bin
 cd ..
 
-git clone https://github.com/unknownskl/greenlight.git
-cd greenlight
-git submodule update --init --recursive
-cd xal-node
+pushd ../xal-node
 cargo update
-cd xal-rs
+pushd xal-rs
 cargo update
-cd ..
-cd ..
+popd
+popd
+
 mkdir deps-dist
-flatpak-node-generator yarn yarn.lock -o ./deps-dist/yarn-sources.json
-flatpak-node-generator npm xal-node/package-lock.json -o ./deps-dist/npm-sources.json
-python3 ${fbtPath}/cargo/flatpak-cargo-generator.py xal-node/Cargo.lock -o ./deps-dist/cargo-sources.json
-cp -f ./deps-dist/* ./flatpak/
-cd flatpak
+flatpak-node-generator yarn yarn.lock -o ./yarn-sources.json
+flatpak-node-generator npm xal-node/package-lock.json -o ./npm-sources.json
+python3 ${fbtPath}/cargo/flatpak-cargo-generator.py xal-node/Cargo.lock -o ./cargo-sources.json
 flatpak-builder --force-clean build-dir ./dev.unknownskl.greenlight.yml
 flatpak build-export export-dir build-dir
 flatpak build-bundle export-dir Greenlight.flatpak dev.unknownskl.greenlight --runtime-repo=https://flathub.org/repo/flathub.flatpakrepo
-cd ..
+
 echo done
