aa99233502adc3a9fbcb83a133904c4ee84f337b
imoc <wzj9912@icloud.com>
2023-11-30 11:01:22 +0800

    new build without rust

--- flatpak/build_bundle_manually.sh
+++ flatpak/build_bundle_manually.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo Run this under flatpak folder. Ensure Rust is installed. Press Enter to continue
+echo Run this under flatpak folder. Press Enter to continue
 read confirm
 sudo pacman -Syu
 sudo pacman -S python-aiohttp python-toml python-pipx flatpak flatpak-builder yarn npm nodejs base-devel git
@@ -12,16 +12,7 @@ pipx install ./node --force
 export PATH=${PATH}:${HOME}/.local/bin
 cd ..
 
-pushd ../xal-node
-cargo update
-pushd xal-rs
-cargo update
-popd
-popd
-
 flatpak-node-generator yarn ../yarn.lock -o ./yarn-sources.json
-flatpak-node-generator npm ../xal-node/package-lock.json -o ./npm-sources.json
-python3 ${fbtPath}/cargo/flatpak-cargo-generator.py ../xal-node/Cargo.lock -o ./cargo-sources.json
 flatpak-builder --force-clean build-dir ./dev.unknownskl.greenlight.yml
 flatpak build-export export-dir build-dir
 flatpak build-bundle export-dir Greenlight.flatpak dev.unknownskl.greenlight --runtime-repo=https://flathub.org/repo/flathub.flatpakrepo
