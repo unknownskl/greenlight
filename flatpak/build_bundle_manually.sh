9fdd2b743f04f3d771f9e07c50edc793259fa170
imoc <wzj9912@icloud.com>
2023-12-23 18:18:30 +0800

    new package id in build script

--- flatpak/build_bundle_manually.sh
+++ flatpak/build_bundle_manually.sh
@@ -13,8 +13,8 @@ export PATH=${PATH}:${HOME}/.local/bin
 cd ..
 
 flatpak-node-generator yarn ../yarn.lock -o ./yarn-sources.json
-flatpak-builder --force-clean build-dir ./dev.unknownskl.greenlight.yml
+flatpak-builder --force-clean build-dir ./io.github.unknownskl.greenlight.yml
 flatpak build-export export-dir build-dir
-flatpak build-bundle export-dir Greenlight.flatpak dev.unknownskl.greenlight --runtime-repo=https://flathub.org/repo/flathub.flatpakrepo
+flatpak build-bundle export-dir Greenlight.flatpak io.github.unknownskl.greenlight --runtime-repo=https://flathub.org/repo/flathub.flatpakrepo
 
 echo done
